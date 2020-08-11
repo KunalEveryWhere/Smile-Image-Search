ff4d4eed77f59a2fe6ddfd64cca07e2d6a85f383
Kunal Prasad <65479883+KunalEveryWhere@users.noreply.github.com>
2020-08-12 00:23:44 +0800

    Add files via upload

---
+++ vanillaJS/main.js
@@ -0,0 +1,41 @@
+const base_API_URL =
+  "https://pixabay.com/api/?key=10144194-4563e940d339119e278421d54&q=";
+
+let imagesSection = document.querySelector("section");
+let loading = document.getElementById("loading");
+let totalHits = document.getElementById("totalHits");
+
+loading.style.display = "none";
+
+let searchTerm = "";
+
+function handleSubmit() {
+  event.preventDefault();
+  searchTerm = "";
+  imagesSection.innerHTML = "";
+  totalHits.innerHTML = "";
+
+  searchTerm = document.getElementById("searchTerm").value;
+
+  console.log(searchTerm);
+
+  loading.style.display = "block";
+  searchImages(searchTerm);
+}
+
+function searchImages(searchTerm) {
+  const constructedURL = base_API_URL + searchTerm + "&image_type=photo";
+  fetch(constructedURL)
+    .then((res) => res.json())
+    .then((data) => showImages(data));
+}
+
+function showImages(data) {
+  totalHits.innerHTML = data.total + " hits!";
+  data.hits.forEach((image) => {
+    const imageElement = document.createElement("img");
+    imageElement.src = image.webformatURL;
+    imagesSection.appendChild(imageElement);
+  });
+  loading.style.display = "none";
+}
